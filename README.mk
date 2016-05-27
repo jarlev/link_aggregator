432a041da0a3faa451009c6b5e01365b383af618
Jared Levin <jaredrlevin@gmail.com>
2016-05-27 14:14:49 -0700

    updated README

---
+++ README.mk
@@ -0,0 +1,9 @@
+Hi, welcome to my link aggregator app!
+
+-With this app, a user can create topics of their choosing. 
+-Once a topic has been created, a user can visit that topic page and create posts within that topic subject. Users can also comment on those posts, once created. 
+-The posts have a voting function - a user can either vote up or down on a post depending on their approval/disapproval. 
+-In this initial version, once a vote has been cast for a post, it cannot be removed or changed. 
+-After a vote has been made, the post's score will update via an AJAX call, and the clicked arrow will temporarily change colors to indicate that the vote was recorded.
+-All posts (regardless of topic) will repopulate on the site's home page '/' in chronological order.
+-Please feel free to message me if you have any questions or suggestions. Thanks for your time!
