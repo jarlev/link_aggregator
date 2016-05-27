3417c90252628854fcba0c845fbb90b220856b54
Jared Levin <jaredrlevin@gmail.com>
2016-05-27 14:37:49 -0700

    updated Postvote migration file

--- db/migrate/20160525184246_change_integer_format_in_my_table.rb
+++ db/migrate/20160525184246_change_integer_format_in_my_table.rb
@@ -1,6 +1,6 @@
 class ChangeIntegerFormatInMyTable < ActiveRecord::Migration
   def change
-    change_column :postvotes, :upvote, :boolean
-    change_column :postvotes, :downvote, :boolean
+    change_column :postvotes, :upvote, 'boolean USING CAST("upvote" AS boolean)'
+    change_column :postvotes, :downvote, 'boolean USING CAST("downvote" AS boolean)'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160525184246) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
@@ -46,8 +49,8 @@ ActiveRecord::Schema.define(version: 20160525184246) do
     t.boolean  "downvote"
   end
 
-  add_index "postvotes", ["post_id"], name: "index_postvotes_on_post_id"
-  add_index "postvotes", ["user_id"], name: "index_postvotes_on_user_id"
+  add_index "postvotes", ["post_id"], name: "index_postvotes_on_post_id", using: :btree
+  add_index "postvotes", ["user_id"], name: "index_postvotes_on_user_id", using: :btree
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -75,7 +78,9 @@ ActiveRecord::Schema.define(version: 20160525184246) do
     t.date     "birthday"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "postvotes", "posts"
+  add_foreign_key "postvotes", "users"
 end
