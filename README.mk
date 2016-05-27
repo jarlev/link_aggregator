618dcd0f951d275bd804afc244101239baaf733f
Jared Levin <jaredrlevin@gmail.com>
2016-05-27 14:42:38 -0700

    added Heroku link to README

--- README.mk
+++ README.mk
@@ -7,3 +7,6 @@ Hi, welcome to my link aggregator app!
 -After a vote has been made, the post's score will update via an AJAX call, and the clicked arrow will temporarily change colors to indicate that the vote was recorded.
 -All posts (regardless of topic) will repopulate on the site's home page '/' in chronological order.
 -Please feel free to message me if you have any questions or suggestions. Thanks for your time!
+
+To view on Heroku:
+https://rocky-badlands-47446.herokuapp.com/
